cd9bd1cc90bfd845ad64197dc7ad8211a927da2f
Simon Frei <simon-frei@hotmail.com>
2021-02-25 18:54:57 +0100

    Add project files.

---
+++ SW01-AllerAnfangIstLeicht/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SW01_AllerAnfangIstLeicht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
