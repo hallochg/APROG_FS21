e2a8a10458c078f7c9a61a79e28d555743e0df15
Simon Frei <simon-frei@hotmail.com>
2021-02-27 09:13:07 +0100

    Add "Programm" Summierer

---
+++ SW01_Console_Summierer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SW01_Console_Summierer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            foreach(string arg in args)
+            {
+                sum += Convert.ToInt32(arg);
+            }
+            Console.WriteLine("Summe: " + sum);
+        }
+    }
+}
