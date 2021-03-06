b0b516e16e096f3b5901876b418b5f6619a2d100
Simon Frei <simon-frei@hotmail.com>
2021-03-06 10:40:48 +0100

    Add Project SW02 Person with class Person and an enum

---
+++ SW02_Person/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW02_Person {
+    class Person {
+        private string mlastname;
+        private string mfirstname;
+        private int mage;
+        private Gender mgender;
+        public string lastname {
+            get { return mlastname; }
+            set { mlastname = value; }
+        }
+
+        public string firstname {
+            get { return mfirstname; }
+            set { mfirstname = value; }
+        }
+
+        public int age {
+            get { return mage; }
+            set { mage = value; }
+        }
+
+        public Gender gender {
+            get { return mgender; }
+            set { mgender = value; }
+        }
+
+
+        public Person(string plastname, string pfirstname, int p_age, Gender pgender) {
+            if (String.IsNullOrEmpty(plastname)) {
+                mlastname = "xxx";
+            } else {
+                mlastname = plastname;
+            }
+            if (String.IsNullOrEmpty(pfirstname)) {
+                mfirstname = "xxx";
+            } else {
+                mfirstname = pfirstname;
+            }
+            mage = p_age;
+            mgender = pgender;
+        }
+
+    }
+}
---
+++ SW02_Person/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SW02_Person {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Person person = new Person("Muster", "Max", 45, Gender.Male);
+            Console.WriteLine($"Nachname: {person.lastname}, Vorname: {person.firstname}, " +
+                $"Alter {person.age}, Geschlecht {person.gender}");
+            Person person_dummy = new Person("",null,100,Gender.Selfdefined);
+            Console.WriteLine($"Nachname: {person_dummy.lastname}, Vorname: {person_dummy.firstname}, " +
+                $"Alter {person_dummy.age}, Geschlecht {person_dummy.gender}");
+        }
+    }
+}
